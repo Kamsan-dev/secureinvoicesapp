e648bb917b0ac146cfe7c9216a8236947df808fe
Kamsan-dev <rim3968@hotmail.fr>
2024-12-23 17:16:54 +0100

    Pipe to calculate the total amount billed to a customer.

--- src/app/components/customers/customer.module.ts
+++ src/app/components/customers/customer.module.ts
@@ -9,9 +9,20 @@ import { NgModule } from '@angular/core';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { ListCustomerComponent } from './list/list-customer.component';
 import { ViewCustomerComponent } from './view/view-customer.component';
+import { ExtractArrayValue } from 'src/app/pipes/extract-value.pipe';
 
 @NgModule({
   declarations: [EditCustomerComponent, ListCustomerComponent, ViewCustomerComponent],
-  imports: [CommonModule, ReactiveFormsModule, SeProgressSpinnerModule, SharedModule, RouterModule, FormsModule, DgdCoreCheckboxModule, CustomerRoutingModule],
+  imports: [
+    ExtractArrayValue,
+    CommonModule,
+    ReactiveFormsModule,
+    SeProgressSpinnerModule,
+    SharedModule,
+    RouterModule,
+    FormsModule,
+    DgdCoreCheckboxModule,
+    CustomerRoutingModule,
+  ],
 })
 export class CustomerModule {}
