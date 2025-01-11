b378f90f45f1205f6178a000db721ab167c65b09
Kamsan-dev <rim3968@hotmail.fr>
2025-01-11 01:13:25 +0100

    upgrade style wip --2

--- src/app/components/stats/stats.module.ts
+++ src/app/components/stats/stats.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { SeProgressSpinnerModule } from 'src/app/common/progress-spinner/se-progress-spinner.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { StatsComponent } from './stats.component';
+import { PanelModule } from 'primeng/panel';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [StatsComponent],
-  imports: [SharedModule, SeProgressSpinnerModule],
+  imports: [SharedModule, SeProgressSpinnerModule, PanelModule, CardModule],
   exports: [StatsComponent],
 })
 export class StatsModule {}
